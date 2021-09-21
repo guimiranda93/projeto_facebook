16d7a75454944e6f624560feb6b0f94224ff267d
guilherme.miranda <guilherme.miranda@CNX-GUILHERMEMIRANDA.local>
2021-09-20 21:05:47 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={[styles.text, styles.text1]}>Hello World</Text>
+      <Text style={[styles.text, styles.text2]}>Hello World 2</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontFamily: 'serif',
+    backgroundColor: 'black',
+  },
+  text1: {
+    color: 'red',
+  },
+  text2: {
+    color: 'green',
+  },
+});
+
+export default styles;
