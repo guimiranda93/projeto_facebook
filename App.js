69348f53b2721a15626cf67d84b77563d6a9dcef
guilherme.miranda <guilherme.miranda@CNX-GUILHERMEMIRANDA.local>
2021-09-20 21:07:15 -0300

    Primeira alteracao

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <View>
       <Text style={[styles.text, styles.text1]}>Hello World</Text>
+
       <Text style={[styles.text, styles.text2]}>Hello World 2</Text>
     </View>
   );
